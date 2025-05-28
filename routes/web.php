a8a638897b343055d2af10db74ed88261982d637
TRẦN ĐỨC DUY <149951157+Ovil8792@users.noreply.github.com>
2025-05-28 08:31:47 +0700

    Đổi lại name của route vì nhầm lẫn

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CustomerController::class,"index"])->name("customer.index");
+Route::get('/', [CustomerController::class,"index"])->name("client.index");
